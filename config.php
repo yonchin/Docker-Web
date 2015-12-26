5b4dd9f92b2450e2e18e6415db755d8b57c7155f
yzh <yonchin@126.com>
2015-12-26 11:22:57 +0800

    更改宿主机地址localhost

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
  date_default_timezone_set('UTC');
  define('ROOT_PATH',dirname(__FILE__));
- define('DOCKER_URL','http://192.168.1.104:2375');
+ define('DOCKER_URL','http://localhost:2375');
 
 spl_autoload_register(function($className){
 	include_once 'libs/'.$className.'.class.php';
